18aacfb0387cddb95da4ad15c2cb909da7beb0e1
Anupam Singh <anupamsingh215@gmail.com>
2024-01-14 05:51:41 +0530

    reponsive navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 import Logo from "../../assets/Rick-and-Morty-Logo.png";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const navigateTo = (link: string) => {
+    navigate(link);
+    setOpenMenu(false);
+  };
+
   return (
-    <div className={styles.navBar}>
-      <div className={styles.logo} onClick={() => navigate("/")}>
-        <img src={Logo} alt="logo" />
+    <div className={`${styles.navBar} ${openMenu ? styles.navBarMobile : ""}`}>
+      <div className={styles.navbarInner}>
+        <div className={styles.logo} onClick={() => navigate("/")}>
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className={styles.navLinks}>
+          <div
+            onClick={() => navigateTo("characters")}
+            className={styles.navLink}
+          >
+            Characters
+          </div>
+          <div
+            onClick={() => navigateTo("locations")}
+            className={styles.navLink}
+          >
+            Locations
+          </div>
+          <div
+            onClick={() => navigateTo("episodes")}
+            className={styles.navLink}
+          >
+            Episodes
+          </div>
+        </div>
+        <div
+          className={styles.hamburgerIcon}
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          {openMenu ? (
+            <span className="material-symbols-outlined">menu_open</span>
+          ) : (
+            <span className="material-symbols-outlined">menu</span>
+          )}
+        </div>
       </div>
-      <div className={styles.navLinks}>
-        <div onClick={() => navigate("characters")} className={styles.navLink}>
-          Characters
+      {openMenu && (
+        <div className={styles.mobileMenu}>
+          <div
+            onClick={() => navigateTo("characters")}
+            className={styles.navLink}
+          >
+            Characters
+          </div>
+          <div
+            onClick={() => navigateTo("locations")}
+            className={styles.navLink}
+          >
+            Locations
+          </div>
+          <div
+            onClick={() => navigateTo("episodes")}
+            className={styles.navLink}
+          >
+            Episodes
+          </div>
         </div>
-        <div onClick={() => navigate("locations")} className={styles.navLink}>
-          Locations
-        </div>
-        <div onClick={() => navigate("episodes")} className={styles.navLink}>
-          Episodes
-        </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ src/utils/useWindowSize .ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+// Custom hook to track window size
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // Function to update window size
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // Add event listener to window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  return windowSize;
+};
+
+export default useWindowSize;
